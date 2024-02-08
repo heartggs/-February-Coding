e524adbc08f15ffe5a300a67dcafd26f5dbf3673
heartggs <jounghj0806@naver.com>
2024-02-08 23:02:42 +0900

    캔버스 지우기 기능

--- src/containers/Nudake.jsx
+++ src/containers/Nudake.jsx
@@ -1,6 +1,11 @@
 import { useEffect, useRef } from "react";
 import "../style/containers/Nudake.css";
 
+import image1 from "../assets/nudake-1.jpg";
+import image2 from "../assets/nudake-2.jpg";
+import image3 from "../assets/nudake-3.jpg";
+import { getAngle, getDistance } from "../utils/utils";
+
 const Nudake = () => {
   const canvasRef = useRef(null);
 
@@ -8,6 +13,10 @@ const Nudake = () => {
     const canvas = canvasRef.current;
     const canvasParent = canvas.parentNode;
     const ctx = canvas.getContext("2d");
+
+    const imageSrcs = [image1, image2, image3];
+    let currentIndex = 0;
+    let prevPos = { x: 0, y: 0 };
 
     let canvasWidth, canvasHeight;
 
@@ -18,13 +27,65 @@ const Nudake = () => {
       canvas.style.height = canvas + "px";
       canvas.width = canvasWidth;
       canvas.height = canvasHeight;
+
+      drawImage();
     }
 
+    function drawImage() {
+      ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+      const image = new Image();
+      image.src = imageSrcs[currentIndex];
+      image.onload = () => {
+        ctx.drawImage(image, 0, 0, canvasWidth, canvasHeight);
+      };
+    }
+
+    function onMousedown() {
+      console.log("onMousedown");
+      canvas.addEventListener("mouseup", onMouseUp);
+      canvas.addEventListener("mouseleave", onMouseUp);
+      canvas.addEventListener("mousemove", onMouseMove);
+      prevPos = { x: e.offsetX, y: e.offsetY };
+    }
+
+    function onMouseUp() {
+      console.log("onMouseUp");
+      canvas.removeEventListener("mouseup", onMouseUp);
+      canvas.removeEventListener("mouseleave", onMouseUp);
+      canvas.removeEventListener("mousemove", onMouseMove);
+    }
+
+    function onMouseMove(e) {
+      console.log("onMouseMove");
+      drawCircles(e);
+    }
+
+    function drawCircles(e) {
+      const nextPos = { x: e.offsetX, y: e.offsetY };
+      const dist = getDistance(prevPos, nextPos);
+      const angle = getAngle(prevPos, nextPos);
+
+      for (let i = 0; i < dist; i++) {
+        const x = prevPos.x + Math.cos(angle) * i;
+        const y = prevPos.y + Math.sin(angle) * i;
+
+        ctx.globalCompositeOperation = "destination-out";
+        ctx.beginPath();
+        ctx.arc(e.offsetX, e.offsetY, 50, 0, Math.PI * 2);
+        ctx.fill();
+        ctx.closePath();
+      }
+
+      prevPos = nextPos;
+    }
+
+    canvas.addEventListener("mousedown", onMousedown);
     window.addEventListener("resize", resize);
     resize();
 
     return () => {
       window.removeEventListener("resize", resize);
+      canvas.removeEventListener("mousedown", onMousedown);
     };
   }, []);
 
